375a592da4a1911196799f94bffcf4b73bf9bad9
WillE <william.fuller90@gmail.com>
2013-06-30 12:25:18 -0500

    #13: Give us an ugly download button

--- public/javascripts/toolbar.js
+++ public/javascripts/toolbar.js
@@ -160,6 +160,17 @@ var Toolbar = oHelpers.createClass(
                 this._oWorkspace.togglePeoplePane();
             }
             
+            // Download document
+            if (jTarget.closest('#download-button').length)
+            {
+                var sHref = window.location.href;
+                if (sHref[-1] != '/')
+                {
+                    sHref += '/'
+                }
+                window.location.href = sHref + 'download';
+            }
+            
             return;                
         }
     },
