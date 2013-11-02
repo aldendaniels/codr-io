c74f0cc2ba93204fac8b67d5e4d2a12ca7ed4619
aldendaniels <ajdaniels5@gmail.com>
2013-11-01 22:54:38 -0500

    Started work on ace build stuff

--- build.js
+++ build.js
@@ -1,10 +1,14 @@
-var requirejs = require('requirejs');
 
+// Delete output dir.
+var oHelpers = require('./helpers-node');
+oHelpers.emptyDirSync(__dirname + '/public/build');
+
+// RequireJS.
+var requirejs = require('requirejs');
 function onBuildError(sError)
 {
     console.log(sError);
 }
-
 requirejs.optimize(
 {
     mainConfigFile: './public/javascripts/require-config.js',
@@ -12,3 +16,11 @@ requirejs.optimize(
     name: 'init-app',
     out: './public/build/init-app.js'
 }, function(){}, onBuildError);
+
+// ACE.
+var oAceBuilder = require('./public/javascripts/edit-control/ace/Makefile.dryice');
+oAceBuilder.buildAce(
+{
+    targetDir: __dirname + '/public/build/ace',
+    compress: true,
+});
