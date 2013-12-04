7b0dad707884929fec8fdee0bdd5f8ab2659c6f4
aldendaniels <ajdaniels5@gmail.com>
2013-12-04 13:15:53 -0600

    Rework app loading
    Don't call require('workspace') until DocReady. Also refactored InitApp
    a bit.

--- public/javascripts/init-app.js
+++ public/javascripts/init-app.js
@@ -5,33 +5,36 @@ define('init-app', function(require)
         oHelpers = require('helpers/helpers-web'),
         oModes   = require('edit-control/modes');
     
-    return {
-        loadModeChooser: function(fnOnModeSelect)
+    var bDocReady = false;
+
+    function loadModeChooser(fnOnModeSelect)
+    {
+        // Init menu.
+        var oMenu = oModes.createModeMenu('#modes', null, function(oMode) // On Mode select
         {
-            // Init menu.
-            var oMenu = oModes.createModeMenu('#modes', this, function(oMode) // On Mode select
-            {
-                fnOnModeSelect(oMode);
-            });
-            oMenu.focusInput();
+            fnOnModeSelect(oMode);
+        });
+        oMenu.focusInput();
+        
+        // Maintain focus.
+        oHelpers.on(window, 'mousedown.home', null, function(oEvent)
+        {
+            if (!$(oEvent.target).is('input, textarea'))
+                oEvent.preventDefault();
+        });
+        
+        // Pass events through to the menu.
+        oHelpers.on(window, 'click.home keyup.home keydown.home', null, function(oEvent)
+        {
+            if ($(oEvent.target).parents('#home #modes').length > 0)
+                oMenu.onEvent(oEvent);
             
-            // Maintain focus.
-            oHelpers.on(window, 'mousedown.home', this, function(oEvent)
-            {
-                if (!$(oEvent.target).is('input, textarea'))
-                    oEvent.preventDefault();
-            });
-            
-            // Pass events through to the menu.
-            oHelpers.on(window, 'click.home keyup.home keydown.home', this, function(oEvent)
-            {
-                if ($(oEvent.target).parents('#home #modes').length > 0)
-                    oMenu.onEvent(oEvent);
-                
-            });        
-        },
-    
-        loadWorkspace: function(bIsNewDocument, bIsSnapshot, oNewDocumentMode)
+        });        
+    }
+        
+    function loadWorkspace(bIsNewDocument, bIsSnapshot, oNewDocumentMode)
+    {
+        var fnLoadWorkspace = function()
         {
             require(['workspace'], function(Workspace, Socket)
             {
@@ -44,23 +47,38 @@ define('init-app', function(require)
                     $('BODY').removeClass('home');
                     $(window).off('.home');
                 }
+            });                
+        };
+        if (this._bDocReady)
+            fnLoadWorkspace()
+        else
+            $(document).ready(fnLoadWorkspace);
+    }
+        
+    return function()
+    {
+        // Store DocReady State.
+        $(document).ready(function()
+        {
+            bDocReady = true;
+        });
+        
+        // Hack: references global variables defined in index.html:
+        //   - IS_NEW_DOCUMENT
+        //   - IS_SNAPSHOT
+        if (IS_NEW_DOCUMENT)
+        {
+            loadModeChooser(function(oMode)
+            {
+                loadWorkspace(IS_NEW_DOCUMENT, IS_SNAPSHOT, oMode)
             });
         }
-    }
+        else
+            loadWorkspace(IS_NEW_DOCUMENT, IS_SNAPSHOT, null);
+    };
 });
 
-require(['init-app'], function(oInitApp)
+require(['init-app'], function(fnInitApp)
 {
-    // Hack: references global variables defined in index.html:
-    //   - IS_NEW_DOCUMENT
-    //   - IS_SNAPSHOT
-    if (IS_NEW_DOCUMENT)
-    {
-        oInitApp.loadModeChooser(function(oMode)
-        {
-            oInitApp.loadWorkspace(IS_NEW_DOCUMENT, IS_SNAPSHOT, oMode)
-        });
-    }
-    else
-        oInitApp.loadWorkspace(IS_NEW_DOCUMENT, IS_SNAPSHOT, null);  
+    fnInitApp();
 });
