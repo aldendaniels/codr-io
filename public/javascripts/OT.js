ce0dfb24a0dcd3dc7eee2f32ccee1d90d649d43b
aldendaniels <ajdaniels5@gmail.com>
2014-01-17 22:37:27 -0600

    Get rid of outdated comment in OT
    The meat of the comment is now document in issue #66.

--- public/javascripts/OT.js
+++ public/javascripts/OT.js
@@ -1,63 +1,3 @@
-/* Thoughts (Nov 24, 2013)
- * 
- * INSERTIONS
- * OT should never modify the size of an insertion range: only the location of that range.
- * The end point of the range will always differ from the start by the number of lines/chars 
- * inserted. Therefore, we should ONLY apply OT to the start point and then move the end 
- * point accordingly.
- *
- * Before, we were separately applying OT to the endpoint. This creates the following (unverified) bug:
- *  DOCUMENT START: ab          state = 0
- *  USER 1:         abC         state = 0 > 1
- *  USER 2:         aTESTb      state = 0 > 2
- *  ------------------------
- *  USER 2 range = 1 - 6 instead of 1 - 5
- *
- * DELETIONS
- * The deletion range should be modified (starting and ending) to reflect previous actions.
- * Additionally, the stored deleted lines should be updating to reflect previous asctions 
- * as well. Up until now, we did not delta the deleted lines. This meant that when the delete
- * action was reverted, the inserted text did not match the text actually deleted post-OT.
- *
- * UNDO / REDO
- * The correction of the above bugs should help insertions.
- *
- * Right now, we dedect groups of similar deltas and undo/redo the entire
- * group together by sequentially undoing/redoing each delta independantly. 
- * This means that that level of undo-granularity differs between pasting in
- * "Hello world" and typing it. 
- *
- * Example (typing):
- *  DOCUMENT START: 1
- *  USER 1:         12
- *  USER 1:         123
- *  USER 1:         1234
- *  USER 2:         124
- *  USER 2:         12_4
- *  USER 1 (undo)   12_ // Undo 4
- *  USER 1 (undo)   12_ // Undo 3, noop, becasue the 3 was already deleted
- *  USER 1 (undo)   1_  // Undo 2
- *
- *  Example (Paste):
- *   DOCUMENT START: 1 
- *   USER 1:         1234 // Paste
- *   USER 2:         124
- *   USER 2:         12_4
- *   USER 1 (undo)   1
- *
- *  We think that the paste-level granularity is really nicer, because the "_" 
- *  left around in the first example is unintuitive. However, on redo, we'd like
- *  the "_" to come back (doesn't happen with paste today). The "Deletions" bug-fix
- *  can help.
- *
- *  On undo/redo, we'd like to construct a single event representing the entire group
- *  (like paste!) Then we'd like to OT the whole thing at once and (thanks to the "deletions" 
- *  fix) we expect the other user's implicated changes to get correctly baked in. Magic!
- *
- *  THIS FILE IS BROKEN. ON PURPOSE!
- * */
-
-
 // Makes importable via node.
 // See http://requirejs.org/docs/node.html
 if (typeof define !== 'function')
@@ -107,7 +47,7 @@ define(function(require)
                             oEnd:   this._getDecrementedPoint(oRange2.oStart, oIntersectEndPoint)
                         }
                     });
-
+                    
                     // Transform deletion range.
                     this.transformRange(oDelta1, oRange2);
                 }
