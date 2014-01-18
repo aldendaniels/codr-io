aldendaniels/codr-io

commit e308aca5b86ada09c458431ebb8b51e8cc41e849

#61: Move the script tag into the body
I got tired of trying to remember how to do a on document ready.

public/javascripts/html-template-dialog.js
@@ -21,6 +21,11 @@ define(function(require)
         '[% if sStyleLanguage == "LESS" %]' +
         '        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.5.0/less.min.js"></script>',
         '[% end %]' +
+        '    </head>',
+        '    <body>',
+        '        ',
+        '        <!-- Your HTML code here. --> ',
+        '        ',
         '[% if sScriptLanguage == "CoffeeScript" %]' +
         '        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/coffee-script/1.6.3/coffee-script.min.js"></script>',
         '[% end %]' +
@@ -34,11 +39,6 @@ define(function(require)
         '            ',
         '        </script>',
         '[% end %]' +
-        '    </head>',
-        '    <body>',
-        '        ',
-        '        <!-- Your HTML code here. --> ',
-        '        ',
         '    </body>',
         '</html>'
     ].join('\n');
@@ -186,6 +186,8 @@ define(function(require)
             // Enable/Disable framework options.
             if ($('select#script-language').val() == 'None')
                 $('select#script-framework').val('None').prop('disabled', true);
+            else
+                $('select#script-framework').val('jQuery').prop('disabled', false);
             
             // Update framework versions dropdown.
             var sSelectedFramework = $('select#script-framework').val();

commit 59178158d6b1f8ab717364b010eb3f03cefb02f6

#61: Fix Script Framework Dropdown
This fixes the bug introduced in e308aca and address the original issue
e308aca sought to correct. Now, when you set your script language back
to "javascript" from none your previous framework selection is restored.

public/javascripts/html-template-dialog.js
@@ -146,6 +146,11 @@ define(function(require)
         _oWorkspace: null,
         _oToolbar: null,
         
+        // Used to restore prov framework selection when the script language
+        // is set back to "Javascript" from "None". 
+        _sLastFrameworkName: '',
+        _sLastFrameworkVersion: '',
+        
         __init__: function(oWorkspace, oToolbar)
         {
             // Save State.
@@ -184,26 +189,50 @@ define(function(require)
         _updateMenuItems: function()
         {
             // Enable/Disable framework options.
+            var jFramework = $('select#script-framework');
+            var jFrameworkVersion = $('select#framework-version');
+            var sSelectedFramework = $('select#script-framework').val();
+            
             if ($('select#script-language').val() == 'None')
-                $('select#script-framework').val('None').prop('disabled', true);
+            {
+                if (sSelectedFramework != 'None')
+                {
+                    this._sLastFrameworkName    = sSelectedFramework;
+                    this._sLastFrameworkVersion = jFrameworkVersion.val();
+                    jFramework.val('None');
+                }
+                jFramework.prop('disabled', true);
+            }
             else
-                $('select#script-framework').val('jQuery').prop('disabled', false);
+            {
+                $('select#script-framework').prop('disabled', false);
+                if (this._sLastFrameworkName)
+                {
+                    jFramework.val(this._sLastFrameworkName);
+                    this._sLastFrameworkName = '';
+                }
+            }
             
             // Update framework versions dropdown.
             var sSelectedFramework = $('select#script-framework').val();
-            var jVersions = $('#framework-version');
-            if (jVersions.data('sFrameworkName') != sSelectedFramework)
+            if (jFrameworkVersion.data('sFrameworkName') != sSelectedFramework)
             {
-                jVersions.data('sFrameworkName', sSelectedFramework);
-                jVersions.empty();
+                jFrameworkVersion.data('sFrameworkName', sSelectedFramework);
+                jFrameworkVersion.empty();
                 if (sSelectedFramework in oFrameworkVersions)
                 {
-                    this._addOptions(jVersions, oFrameworkVersions[sSelectedFramework], 'Version ');
-                    jVersions.prop('disabled', false);
+                    this._addOptions(jFrameworkVersion, oFrameworkVersions[sSelectedFramework], 'Version ');
+                    jFrameworkVersion.prop('disabled', false);
+                    
+                    if (this._sLastFrameworkVersion)
+                    {
+                        jFrameworkVersion.val(this._sLastFrameworkVersion);
+                        this._sLastFrameworkVersion = '';
+                    }
                 }
                 else
-                    jVersions.prop('disabled', true);
-            }
+                    jFrameworkVersion.prop('disabled', true);
+            }            
         },
         
         _addOptions: function(jSelect, aOptions, sOptionalsPrefix)