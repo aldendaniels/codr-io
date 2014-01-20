4d7f4b2d1f4b1b4da85dff15dbe3633c66626db4
aldendaniels <ajdaniels5@gmail.com>
2014-01-20 15:21:27 -0600

    Closes #39: Fix nasty Chrome bug
    See the extensive comment in init-app.js about this nasty Chrome bug.
    This was causing codr.io to refresh at a frenetic pace whenever you
    accessed it from Chrome.
    
    The change to index.html is an unrelated removal of illegal </input>
    tags.

--- public/javascripts/init-app.js
+++ public/javascripts/init-app.js
@@ -54,26 +54,16 @@ define('init-app', function(require)
         else
             $(document).ready(fnLoadWorkspace);
     }
-        
+    
     return function()
-    {
-        // Reload whever the url changes.
-        // TODO: Ideally we wouldn't reload when clicking "back"
-        //       after creating a document. We could just hide the document
-        //       and show the landing page. This takes some work, however,
-        //       so for now we just reload.
-        oHelpers.on(window, 'popstate', this, function(e)
-        {
-            window.location.reload();
-        });
-        
+    {        
         // Store DocReady State.
         $(document).ready(function()
         {
-            bDocReady = true;
+            bDocReady = true;            
         });
-        
-        // Hack: references global variables defined in index.html:
+
+        // References global variables defined in index.html:
         //   - IS_NEW_DOCUMENT
         //   - IS_SNAPSHOT
         if (IS_NEW_DOCUMENT)
@@ -88,6 +78,33 @@ define('init-app', function(require)
     };
 });
 
+// Reload whever the url changes.
+//
+//      TODO:           Ideally we wouldn't reload when clicking "back" after creating a document.
+//                      We could just hide the document and show the landing page, which would be faster.
+//                      This takes some work, however, so for now we just reload.
+//
+//      Chrome Bug:     Chrome erroneously fires the popstate event on page load.
+//                      This bug has been fixed, but the fix is not yet in production.
+//                      See: https://code.google.com/p/chromium/issues/detail?id=63040
+//
+//      Chrome Bug Fix: Using the onload + timeout trick silences the buggy popstate event in Chrome.
+//                      This is a variant of the fix proposed by Torben.
+//                      See: http://stackoverflow.com/questions/6421769/popstate-on-pages-load-in-chrome/10651028#10651028
+//
+//      NOTE:            Once the Chrome bug fix has been out in the wild for a bit, we can simplify this code.
+window.addEventListener('load', function()
+{
+    window.setTimeout(function()
+    {
+        window.addEventListener('popstate', function()
+        {
+            window.location.reload();
+        });
+    }, 0);
+});
+
+// Start App.
 require(['init-app'], function(fnInitApp)
 {
     fnInitApp();
